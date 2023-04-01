4f4f4da1b26aa5edb611d0f571b3770584980389
commi235 <moritz.klein@it-twins.de>
2023-04-01 20:16:21 +0200

    Page Items with NULL get ignored

--- src/js/mtag.grid.masschange.js
+++ src/js/mtag.grid.masschange.js
@@ -3,54 +3,63 @@
 var mtag = mtag || {};
 mtag.grid = mtag.grid || {};
 
-(function($, region, item, grid){
-
-  function _exec( pRegionId, pItems, pColumns, pDoSave ) {
-    var lRegion = region( pRegionId );
-    var lCurrentModel = lRegion.call( "getCurrentView" ).model;
-    if ( !lCurrentModel ) {
-      console.error( "No model found on region!", lRegion);
+(function ($, region, item, grid) {
+  function _exec(pRegionId, pItems, pColumns, pDoSave) {
+    var lRegion = region(pRegionId);
+    var lCurrentModel = lRegion.call("getCurrentView").model;
+    if (!lCurrentModel) {
+      console.error("No model found on region!", lRegion);
     } else {
-      var lSelectedRecords = lRegion.call( "getSelectedRecords" );
+      var lSelectedRecords = lRegion.call("getSelectedRecords");
 
-      if ( lSelectedRecords.length > 0 ) {
-
+      if (lSelectedRecords.length > 0) {
         var lItemValues = [];
         for (let index = 0; index < pItems.length; index++) {
-          lItemValues[index] = item( pItems[index] ).getValue();          
+          lItemValues[index] = item(pItems[index]).getValue();
         }
 
         for (let index = 0; index < lSelectedRecords.length; index++) {
-          let lRecordId = lCurrentModel.getRecordId( lSelectedRecords[index] );
+          let lRecordId = lCurrentModel.getRecordId(lSelectedRecords[index]);
           for (let index = 0; index < pColumns.length; index++) {
-            if ( lCurrentModel.getFieldKey( pColumns[index] ) ) {
-              lCurrentModel.setRecordValue( lRecordId, pColumns[index], lItemValues[index]);              
+            if (lCurrentModel.getFieldKey(pColumns[index])) {
+              if (lItemValues[index] != "") {
+                lCurrentModel.setRecordValue(
+                  lRecordId,
+                  pColumns[index],
+                  lItemValues[index]
+                );
+              }
             } else {
-              console.warn( "Unknown field.", pColumns[index] );
+              console.warn("Unknown field.", pColumns[index]);
             }
           }
         }
       } else {
-        console.warn( "No selected records found." );
+        console.warn("No selected records found.");
       }
-      if ( pDoSave ) {
-        lRegion.call( "getActions" ).invoke( "save" );
+      if (pDoSave) {
+        lRegion.call("getActions").invoke("save");
       }
     }
   }
 
-  grid.masschange = function() {
+  grid.masschange = function () {
+    var lRegionId = this.action.affectedRegionId,
+      lItems = this.action.attribute01.split(","),
+      lAutoMap = this.action.attribute02, // only included to have complete settings. Not used.
+      lColumns = this.action.attribute03.split(","),
+      lDoSave = this.action.attribute04 == "Y";
 
-    var lRegionId =   this.action.affectedRegionId,
-        lItems    =   this.action.attribute01.split(","),
-        lAutoMap  =   this.action.attribute02, // only included to have complete settings. Not used.
-        lColumns  =   this.action.attribute03.split(","),
-        lDoSave   = ( this.action.attribute04 == "Y" );
-
-    apex.debug.trace( "Settings used", lRegionId, lItems, lAutoMap, lColumns, lDoSave );
-    _exec( lRegionId, lItems, lColumns, lDoSave );
-    
-  }
+    apex.debug.trace(
+      "Settings used",
+      lRegionId,
+      lItems,
+      lAutoMap,
+      lColumns,
+      lDoSave
+    );
+    _exec(lRegionId, lItems, lColumns, lDoSave);
+  };
 
   mtag.grid = grid;
-})(apex.jQuery, apex.region, apex.item, mtag.grid)
+})(apex.jQuery, apex.region, apex.item, mtag.grid);
